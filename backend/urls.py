34dcb058298e31771c95cbf46bb621683d17ce76
apl_1978 <apl_1978@yahoo.com>
2023-02-15 11:18:46 +0300

    Added PartnerUpdate view and requests-examples.http.

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from backend.views import ShopView, CategoryView
+from backend.views import ShopView, CategoryView, PartnerUpdate
 
 app_name = 'backend'
 urlpatterns = [
     path('shops', ShopView.as_view(), name='shops'),
-path('categories', CategoryView.as_view(), name='categories'),
+    path('categories', CategoryView.as_view(), name='categories'),
+    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
 ]
